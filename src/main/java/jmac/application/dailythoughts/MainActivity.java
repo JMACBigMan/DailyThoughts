fb4505acb7a858864845fa66b4106d3de5f4e382
JMACBigMan <jemclaurin@gmail.com>
2020-07-26 09:34:22 -0400

    Initial commit

---
+++ src/main/java/jmac/application/dailythoughts/MainActivity.java
@@ -0,0 +1,113 @@
+package jmac.application.dailythoughts;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private EditText enterTitle;
+    private EditText enterThought;
+    private Button saveButton, showButton;
+    private TextView recTitle, recThought;
+
+
+    //Keys
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_THOUGHT = "thought";
+
+    // Connection to FireStore...
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference journalRef = db.collection("Journal")
+            .document("First Thoughts");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        saveButton = findViewById(R.id.save_button);
+        enterTitle = findViewById(R.id.edit_text_title);
+        enterThought = findViewById(R.id.edit_text_thoughts);
+        recThought = findViewById(R.id.rec_thought);
+        recTitle = findViewById(R.id.rec_title);
+        showButton = findViewById(R.id.show_dataID);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick( View view) {
+                journalRef.get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                if (documentSnapshot.exists()) {
+                                    String title = documentSnapshot.getString(KEY_TITLE);
+                                    String thought = documentSnapshot.getString(KEY_THOUGHT);
+
+                                    recTitle.setText(title);
+                                    recThought.setText(thought);
+
+                                }else {
+                                    Toast.makeText(MainActivity.this,
+                                            "No data exists",
+                                            Toast.LENGTH_LONG)
+                                            .show();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                            }
+                        });
+
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = enterTitle.getText().toString().trim();
+                String thought = enterThought.getText().toString().trim();
+
+                Map <String, Object> data = new HashMap <> ();
+                data.put(KEY_TITLE, title);
+                data.put(KEY_THOUGHT, thought);
+
+                journalRef.set(data)
+                        .addOnSuccessListener(new OnSuccessListener <Void> () {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener () {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: " + e.toString());
+                            }
+                        });
+
+            }
+        });
+    }
+}
